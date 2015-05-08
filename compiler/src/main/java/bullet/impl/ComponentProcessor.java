d6e2e4ab32514d6932387238905c3237162489a8
Thomas Broyer <t.broyer@ltgt.net>
2015-05-08 20:07:14 +0200

    Oops! Removed too much when switching to BasicAnnotationProcessor

--- compiler/src/main/java/bullet/impl/ComponentProcessor.java
+++ compiler/src/main/java/bullet/impl/ComponentProcessor.java
@@ -3,12 +3,19 @@ package bullet.impl;
 import java.util.Collections;
 
 import javax.annotation.processing.Processor;
+import javax.lang.model.SourceVersion;
 
 import com.google.auto.common.BasicAnnotationProcessor;
 import com.google.auto.service.AutoService;
 
 @AutoService(Processor.class)
 public class ComponentProcessor extends BasicAnnotationProcessor {
+
+  @Override
+  public SourceVersion getSupportedSourceVersion() {
+    return SourceVersion.latestSupported();
+  }
+
   @Override
   protected Iterable<? extends ProcessingStep> initSteps() {
     return Collections.singleton(new ComponentProcessingStep(processingEnv));
